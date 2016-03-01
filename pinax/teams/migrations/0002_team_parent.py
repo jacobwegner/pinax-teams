981e561e6184fd66e5121b3c9c4b0901c6114aa0
Jacob Wegner <jacobwegner@gmail.com>
2016-03-01 09:30:15 -0600

    update migration 0002 to use pinax_teams app label

--- pinax/teams/migrations/0002_team_parent.py
+++ pinax/teams/migrations/0002_team_parent.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='team',
             name='parent',
-            field=models.ForeignKey(related_name='children', blank=True, to='teams.Team', null=True),
+            field=models.ForeignKey(related_name=b'children', blank=True, to='pinax_teams.Team', null=True),
         ),
     ]
